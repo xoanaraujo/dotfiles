696a82e0764fc678390a31c037e03550cf97fc1a
xoanaraujo <xoanaraujodev@gmail.com>
2025-03-07 21:46:05 +0100

    batman

--- nvim/lua/xoanaraujo/config/options.lua
+++ nvim/lua/xoanaraujo/config/options.lua
@@ -8,8 +8,8 @@ opt.number = true
 opt.wrap = false
 
 -- tabs & indentation
-opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 opt.scrolloff = 8
